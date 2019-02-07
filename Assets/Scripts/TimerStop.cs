ffd04573ce2b2e1d4b5582104eb3bcf92970ab7c
Ryan <ryank1030@gmail.com>
2019-02-07 10:53:44 -0600

    Added Timer Stop
    Added an asset with a timer stop script attached to stop the timer when the player collides with the asset.

---
+++ Assets/Scripts/TimerStop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerStop : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Text").SendMessage("Finish");
+    }
+}
