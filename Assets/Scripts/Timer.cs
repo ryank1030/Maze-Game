6fdd26be9887ad48bf8ae1526180f6ee3bd52777
Ryan <ryank1030@gmail.com>
2019-02-07 10:09:24 -0600

    Added Timer
    Timer counts up since start of game.
    Nothing is stopping the timer right now.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,19 +7,34 @@ public class Timer : MonoBehaviour
 {
 
     private float timer = 0.0F;
-    public bool logging = false;
-    public Text txt;
+    public bool finished = false;
+    public Text timerTxt;
+
+    private void Start()
+    {
+        timer = Time.time;  //initilize the timer
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-        timer = Time.timeSinceLevelLoad;
-        txt.text = "Timer: " + timer.ToString();
-
-        if (logging)
+        if (finished)
         {
-            Debug.Log(txt.text);
+            return;
         }
+
+        float t = Time.time - timer; //grab the difference
+        string minutes = ((int)t / 60).ToString();  //get minutes 
+        string seconds = (t % 60).ToString("f0"); //get seconds and remove milisecond
+
+        timerTxt.text = "Timer: " + minutes + ":" + seconds;  //display the timer
+    }
+
+    //to run this send a message Finish
+    //GameObject.Find("xxx").SendMessage("Finish")
+    public void Finish()  
+    {
+        finished = true;
+        timerTxt.color = Color.green;
     }
 }
